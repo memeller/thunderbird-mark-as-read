bd2bcd6fe6ccc80d9a6709ae62dd487f25769f83
me <memeller@gmail.com>
2024-01-16 11:38:24 +0100

    Fixed #1, small fixes
    Added option to change folder selection method (automatically include subfolders / select single nodes) - #1
    Settings are now loaded all at once

--- src/view/options.vue
+++ src/view/options.vue
@@ -3,6 +3,8 @@
 		<h1>Select folders</h1>
 		<p>Messages in selected folders will be marked as read after each new mail is received and also on each program launch.</p>
 		<tree name="tree" :nodes="nodes" :config="config" @nodeChecked="nodeChanged" @nodeUnchecked="nodeChanged"></tree>
+		<label for="checkbox_tree"><input type="checkbox" id="checkbox_tree" v-model="treeSelectionMode"/>
+		Auto select/deselect subfolder nodes when parent folder is selected.</label>
 		<p>Changes are automatically saved on each modification</p>
 		
 		<p>If you wish to mark existing messages in these folders as read now, you can do so by using the button below.</p>
@@ -23,10 +25,9 @@
 			tree: treeview,
 		},
 		mounted() {
-			this.config.checkMode=0;
 			browser.accounts.list().then(this.listAccounts);
-			let getting = browser.storage.sync.get("selectedKeys");
-			getting.then(this.onKeysLoaded, this.onError);
+			let getting = browser.storage.sync.get(["selectedKeys","treeSelectionMode","useFolderInfoEvent"]);
+			getting.then(this.onSettingsLoaded.bind(this), this.onError);
 			
 		},
 		watch: {
@@ -34,6 +35,12 @@
 				browser.storage.sync.set({
 					useFolderInfoEvent: newValue
 				});
+			},
+			treeSelectionMode(newValue){
+				browser.storage.sync.set({
+					treeSelectionMode: newValue
+				});
+				this.config.checkMode=newValue?0:1;
 			}
 		},
 		methods: {
@@ -84,25 +91,26 @@
 						);
 				});
 			},
-			onKeysLoaded:function(result)
+			onSettingsLoaded:function(result)
 			{
 				
-				if (!("selectedKeys" in result)) return;
 				
-					this.selectedNodes = result.selectedKeys;
-				
-				let that=this;
-				Object.values(this.selectedNodes).forEach(function(value) {
-					return that.nodes[value].state.checked = true;
-				});
-				let getting = browser.storage.sync.get("useFolderInfoEvent");
-				getting.then(this.onUseFolderInfoLoaded, this.onError);
-			},
-			onUseFolderInfoLoaded:function(result)
-			{
-				if (!("useFolderInfoEvent" in result)) return;
+				if ("useFolderInfoEvent" in result)
 					this.useOnFolderInfo = result.useFolderInfoEvent;
-				
+				if ("treeSelectionMode" in result)
+				{
+					this.treeSelectionMode = result.treeSelectionMode;
+					this.config.checkMode=this.treeSelectionMode?0:1;
+				}
+				if ("selectedKeys" in result)
+				{
+					this.selectedNodes = result.selectedKeys;
+					
+					let that=this;
+					Object.values(this.selectedNodes).forEach(function(value) {
+						return that.nodes[value].state.checked = true;
+					});
+				}
 			},
 			onError:function(error)
 			{
@@ -128,10 +136,12 @@
 						viewBox: "0 0 24 24",
 						draw: `M 12 2 L 12 22 M 2 12 L 22 12`,
 					},
+					checkMode:0
 				},
 				nodes: {},
 				selectedNodes:[],
-				useOnFolderInfo:false
+				useOnFolderInfo:false,
+				treeSelectionMode:1
 			};
 		},
 	};
