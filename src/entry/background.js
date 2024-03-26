82e6892a4c849634d3fc27dfa56146ddbe56b32b
me <memeller@gmail.com>
2024-03-26 12:44:16 +0100

    fix bug where event listener might not be added
    Fix issue where an event listener might not be added to event if settings value is not set
    Added message move event handler

--- src/entry/background.js
+++ src/entry/background.js
@@ -35,26 +35,37 @@ function onOptionsLoaded(options,onChanged=false) {
         selectedFolders = getValueFromStorageObj(options,"selectedKeys",selectedFolders);
     if(options.useFolderInfoEvent!==undefined)
     {
-        useFolderInfoEvent  = getValueFromStorageObj(options,"useFolderInfoEvent",useFolderInfoEvent);    
-        if (useFolderInfoEvent) {
-            if(logConsole)
-                console.debug("MarkAsRead: Using folder info event for new mail tracking");
-            browser.folders.onFolderInfoChanged.addListener(folderInfoChanged);
-            browser.messages.onNewMailReceived.removeListener(messageReceivedListener);
-        } else {
-            if(logConsole)
-                console.debug("MarkAsRead: Using new mail received event for new mail tracking");
-            browser.messages.onNewMailReceived.addListener(messageReceivedListener);
-            browser.folders.onFolderInfoChanged.removeListener(folderInfoChanged);
-        }   
+        let useFolderInfoEventNewValue  = getValueFromStorageObj(options,"useFolderInfoEvent",useFolderInfoEvent);            
+        if(useFolderInfoEvent!=useFolderInfoEventNewValue)
+        {
+            useFolderInfoEvent=useFolderInfoEventNewValue;
+            setEventHandlers();
+        }
     }
     if(isStartup)
     {
         isStartup=false;
+        setEventHandlers();   
         if(logConsole)
             console.debug("MarkAsRead: Scanning folders at startup");
         scanAndMarkAsRead(selectedFolders);
     }
+}
+function setEventHandlers()
+{
+    if (useFolderInfoEvent) {
+        if(logConsole)
+            console.debug("MarkAsRead: Using folder info event for new mail tracking");
+        browser.folders.onFolderInfoChanged.addListener(folderInfoChanged);
+        browser.messages.onNewMailReceived.removeListener(messageReceivedListener);
+        browser.messages.onMoved.removeListener(messageMovedListener);
+    } else {
+        if(logConsole)
+            console.debug("MarkAsRead: Using new mail received event for new mail tracking");
+        browser.messages.onNewMailReceived.addListener(messageReceivedListener);
+        browser.folders.onFolderInfoChanged.removeListener(folderInfoChanged);
+        browser.messages.onMoved.addListener(messageMovedListener);    
+    }   
 }
 function getValueFromStorageObj(storageObj,key,defaultValue)
 {
@@ -80,7 +91,38 @@ function folderInfoChanged(folder, folderInfo) {
         console.debug(`MarkAsRead: Folder info change detected, checking if anything should be marked as read`);
     markAsReadFolderData(folder, selectedFolders);
 }
+async function messageMovedListener(oldMessages, movedMessages) {
+    if(logConsole)
+        console.debug(`MarkAsRead: MessagemovedEvent`);
+    movedMessages.forEach(message => {
+        if(logConsole)
+            console.log(message);
+        //folder is optional in messageHeader
+        if(message.folder===undefined)
+        {
+           return;
+        }
+        else
+        {
+            let folder=message.folder;
+            let tempId = md5(folder.accountId + folder.path);
+            if(logConsole)
+                console.debug(`MarkAsRead: New message detected, tempId: ${tempId}`);
+            if (selectedFolders.includes(tempId)) {
+                if(logConsole)
+                    console.debug(`MarkAsRead: Message belongs to folder that is marked`);
+                if(logConsole)
+                    console.debug(`MarkAsRead: Marking message ${message.id} as read`);
+                browser.messages.update(message.id, { read: true });
+        
+            }
+        }
+    });
+
+}
 async function messageReceivedListener(folder, messages) {
+    if(logConsole)
+    console.debug(`MarkAsRead: messageReceivedListener`);
     let tempId = md5(folder.accountId + folder.path);
     if(logConsole)
         console.debug(`MarkAsRead: New message detected, tempId: ${tempId}`);
