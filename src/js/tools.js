b73c6e000ac85b39f79ead03c00494b617c18ea7
me <memeller@gmail.com>
2024-04-16 15:28:25 +0200

    bugfixes, packages updated
    - Fixed a bug, where if message list contained more than 100 messages only first 100 messages were marked as read
    - Added initial support for folders.markAsRead in Thunderbird 121
    - Added some formatting to debug output
    - Updated packages

--- src/js/tools.js
+++ src/js/tools.js
@@ -1,6 +1,8 @@
 /*jshint esversion: 8 */
 var md5 = require("md5");
 var logConsole=false;
+const info = await browser.runtime.getBrowserInfo();
+    
 export async function scanAndMarkAsRead(selectedFolders) {
     if(logConsole)
         console.debug(`MarkAsRead: scanAndMarkAsRead`)
@@ -13,7 +15,7 @@ export async function scanAndMarkAsRead(selectedFolders) {
 
 export function markAsReadFolderData(folderData, markAsReadIds) {
     if(logConsole)
-        console.debug(`MarkAsRead: markAsReadFolderData`)
+        console.debug(`MarkAsRead: + markAsReadFolderData`)
     if (Array.isArray(folderData)) {
         folderData.forEach((folder) => {
             checkFolderAndMark(folder, markAsReadIds);
@@ -29,21 +31,43 @@ export function setDebug(isDebug)
     if(logConsole)
         console.debug("MarkAsRead: tools setDebug: "+isDebug);
 }
-function checkFolderAndMark(folder, markAsReadIds) {
+async function checkFolderAndMark(folder, markAsReadIds) {
     let id = md5(folder.accountId + folder.path);
     if(logConsole)
-        console.debug(`MarkAsRead: check folders and mark as read: ${folder.accountId} ${folder.path}`)
+        console.debug(`MarkAsRead: | check folders and mark as read: ${folder.accountId} ${folder.path}`)
     if (markAsReadIds.includes(id))
     {
         if(logConsole)
-            console.debug(`MarkAsRead: Found id that should be marked: ${id}`)
-        browser.messages
-        .query({ unread: true, folder: folder })
-        .then((messageList) => {
-            if (messageList.messages.length > 0)
-                messageList.messages.forEach((message) => {
-                    browser.messages.update(message.id, { read: true });
-                });
-        });
+            console.debug(`MarkAsRead: |__ Found id that should be marked: ${id}`)
+        if(parseInt(info.version) >=111 && typeof("browser.folders.markAsRead")==="function")
+        {
+            browser.folders.markAsRead(folder);
+            if(logConsole)
+                console.debug(`MarkAsRead:    + Using new method`)
+        }
+        else
+        {
+            if(logConsole)
+                console.debug(`MarkAsRead:    + Using old method`)
+            
+            let messages = getMessages(browser.messages.query({ unread: true, folder: folder }));
+            for await (let message of messages) {
+                browser.messages.update(message.id, { read: true });
+            }
+        }
     }
 }
+async function* getMessages(list) {
+    let page = await list;
+    console.log(page);
+    for (let message of page.messages) {
+      yield message;
+    }
+  
+    while (page.id) {
+      page = await browser.messages.continueList(page.id);
+      for (let message of page.messages) {
+        yield message;
+      }
+    }
+  }
